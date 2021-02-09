a00d32f74f8f1d394969e80c8a3ab6bfb0641c85
chris-aeviator <chris@myzel.io>
2021-02-09 14:07:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentinelloader',
-    version='1.1.1-01',
-    url='https://github.com/chris-aeviator/sentinelloader/edit/master/setup.py',
+    version='1.1.2',
+    url='https://github.com/chris-aeviator/sentinelloader.git',
     author='Flávio Stutz',
     license='MIT',
     author_email= 'flaviostutz@gmail.com',
